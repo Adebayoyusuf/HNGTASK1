a605eaf93cbeffa3f55bdca0e6bc62f60253c3b9
Adebayo Yusuf <offisial007@outlook.com>
2022-10-31 22:56:00 +0100

    update

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
-from flask import Flask, jsonify
 from flask_pydantic import validate
 from pydantic import BaseModel
-from dataclasses import dataclass
+from flask import Flask
 
-app = Flask(__name__)
+schema = {
+    'type': 'object',
+    'properties': {
+        'name': {'type': 'string'},
+        'email': {'type': 'string'},
+        'password': {'type': 'string'}
+    },
+    'required': ['email', 'password']
+}
 
 
-class BioValidator(BaseModel):
-    slackUserName : str
+class BioData(BaseModel):
+    slackUserName: str
     Backend: bool
     Age: int
     bio: str
 
+app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 @validate()
-def index():
-    slack = "offisial"
-    back = True
-    age=20
-    bio = "I am a Python Flask Backend developer"
-    bio = BioValidator(
-        slackUserName=slack,
-        Backend=True,
-        Age=age,
-        bio=bio
-    )
+def get():
+    new_profile = {
+    "slackUserName": "offisial",
+    "Backend": True,
+    "Age":20,
+    "bio":"I am backend developer"
+    }
+    response = BioData(**new_profile)
+    return response
 
-    return bio
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=8080) 
-
-
-
-      
-
-
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
